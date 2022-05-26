296e3a36d9616cfa039b768caf29e1a6ea394410
MDC Service <michael.schmid@mdc-service.de>
2022-05-26 10:13:27 +0200

    check in SD-card drivers and test

---
+++ software/drivers/sdcard_funcs.hpp
@@ -0,0 +1,15 @@
+/*
+ * sdcard_funcs.hpp
+ *
+ *  Created on: 26.02.2022
+ *      Author: michi
+ *  SPDX-FileCopyrightText: 2022 MDC Service <info@mdc-service.de>
+ *  SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef _SDCARD_FUNCS_H_
+#define _SDCARD_FUNCS_H_
+
+void test_sdcard(void);
+
+#endif
